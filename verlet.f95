dc2cce629db3798ed9194ce1500d0a4d9ba1680e
hungiro <hungironehemia@gmail.com>
2025-11-28 07:29:38 +0100

    Exercise 1

--- verlet.f95
+++ verlet.f95
@@ -1,16 +1,41 @@
 PROGRAM verlet
   IMPLICIT NONE
-  INTEGER, PARAMETER :: wp = SELECTED_REAL_KIND (p=13, r=300)
-  INTEGER :: i, j, k
-  INTEGER :: l, m
-  CHARACTER (LEN=72) :: str1, str2, str3
-  REAL (KIND=wp), DIMENSION(:), ALLOCATABLE :: x
-  REAL (KIND=wp), DIMENSION(:,:), ALLOCATABLE :: xmat
   
-  ALLOCATE ( x (300) )
-  ALLOCATE ( xmat (200, 300) )
-  x(3)=4.0_wp
-  PRINT *, x(3)
+  ! Parameters: Mass (m), Time Step (tau), Total Steps (N)
+  REAL, PARAMETER :: m = 1.0, tau = 0.2
+  INTEGER, PARAMETER ::  n = 600
+
+  ! Constant Force Components
+  REAL, PARAMETER :: fx = 0.0, fy = 0.1, fz = 0.0
+
+  ! State Variables (Position and Velocity)
+  REAL :: x = 0.0, y = 0.0, z = 0.0
+  REAL :: vx = 0.0, vy = 0.0, vz = 0.0
+
+  ! Loop counter and Time
+  INTEGER :: k
+  real :: time_k = 0.0
+
+  PRINT *, 'Time (s), X (m), Y (m), Z (m)'
+  PRINT *, time_k, x, y, z
+
+  ! Main Simulation Loop
+  DO k = 1, n
+  time_k = time_k + tau
+
+  ! New Position
+  x = x + tau * vx + (tau**2 / (2.0 * m)) * fx
+  y = y + tau * vy + (tau**2 / (2.0 * m)) * fy
+  z = z + tau * vz + (tau**2 / (2.0 * m)) * fz
+
+  ! New Velocities
+  vx = vx + (tau / m) * fx
+  vy = vy + (tau / m) * fy
+  vz = vz + (tau / m) * fz
+
+  PRINT *, time_k, x, y, z
+
+  END DO
   
 END PROGRAM verlet
 
